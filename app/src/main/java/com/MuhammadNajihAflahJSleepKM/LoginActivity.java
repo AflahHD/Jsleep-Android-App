a8cc1b2ba3c203b4113767ca77e85aadc0167602
Muhammad Najih Aflah <muhammad.najih11@ui.ac.id>
2022-11-15 18:07:54 +0700

    CS Modul 8

---
+++ app/src/main/java/com/MuhammadNajihAflahJSleepKM/LoginActivity.java
@@ -0,0 +1,34 @@
+package com.MuhammadNajihAflahJSleepKM;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        TextView register = findViewById(R.id.RegisterNowButton);
+        TextView mainActivity = findViewById(R.id.LoginButton);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent move = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(move);
+            }
+        });
+
+        mainActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent move = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(move);
+            }
+        });
+    }
+}
